15ebfad12ddee7e61a7b73b4fe724de7e7ea9289
tbrankov <tbrankovic@juniorisep.com>
2021-03-13 15:28:45 +0100

    Modification de members.json Ajouté des icônes

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,11 +1,14 @@
 export const na = require("./na.png")
 export const cc = require("./cc.png")
 export const fa = require("./fa.png")
+export const tb = require("./tb.png")
+
 export const leftArrow = require("./left-arrow.png")
 
 export const images = {
     'na': require("./na.png"),
     'cc': require("./cc.png"),
     'fa': require("./fa.png"),
+    'tb': require("./tb.png"),
     'leftArrow': require("./left-arrow.png")
 }
--- screens/Details.js
+++ screens/Details.js
@@ -7,14 +7,14 @@ const Details = ({ route }) => {
     const email = (name.charAt(0) + name.substr(name.indexOf(' ') + 1) + '@juniorisep.com').toLowerCase()
 
     return (
-        <View style={styles.container}>
-            <Image style={ styles.profile }
-                   source={ images[route.params.image] }
+        <View style={ styles.container }>
+            <Image style={styles.profile}
+                   source={images[route.params.image]}
             />
-            <Text style={styles.largeTitle}>{ name }</Text>
+            <Text style={styles.largeTitle}>{name}</Text>
             <Button /*color='#22CC10'*/
-                    onPress={() => Linking.openURL('mailto:' + email) }
-                    title={ email }
+                    onPress={() => Linking.openURL('mailto:' + email)}
+                    title={email}
             />
             <Text style={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
         </View>
@@ -40,7 +40,6 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold'
     },
-
     paragraph: {
         fontSize: 16,
         paddingTop: 24
